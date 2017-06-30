589702b7ca4399d7605a0728f34c63f84adfc650
SHEN CHENHUI <sch88180733@163.com>
2017-06-30 15:00:55 -0700

    structural cleanup

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var dummyData=["laundry","clean the floor","cooking"];
+var dummyData=[{ taskText: "laundry", completed: false },{ taskText: "Catch 'em all", completed: true },{ taskText: "cooking", completed: false }];
 
 function Todo(props){
   return (
-    <li><button>X</button> {props.task}</li>
+    props.completed? <li><button className="btn-danger">X</button> <strike>{props.task}</strike></li>:
+    <li><button className="btn-danger">X</button> {props.task}</li>
+    //don't put button outside because it is inside <ul> which looks for <li>
   )
 };
-// class Todo extends React.Component{
-//   render(){
-//     return (
-//         <li>{this.props.task}<button>X</button></li>
-//     )
-//   }
-// }
-class TodoList extends React.Component {
+function InputLine(props){
+  return (
+    <div>
+      <input type="text" placeholder="task"/>
+      <button className="btn-info">Add todo</button>
+    </div>
+  )
+};
+
+function TodoList(props){
+  return (
+    <ul>{props.todos.map(task=><Todo task={task.taskText} completed={task.completed}/>)}</ul>
+  )
+};
+
+class TodoApp extends React.Component{
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+  componentDidMount(){
+    this.setState({todos: dummyData})
   }
   render(){
-    return (
-      <ul>{dummyData.map(task=><Todo task={task}/>)}</ul>
+    return(
+      <div>
+        <InputLine />
+        <TodoList todos={this.state.todos} />
+      </div>
     )
   }
 }
 
-ReactDOM.render(<TodoList />,
+ReactDOM.render(<TodoApp />,
    document.getElementById('root'));
